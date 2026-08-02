c147330ae99233f98b96ebd0e254d0239a04538b
Mr-Soproonge <mrspoogeogegege@tutamail.com>
2026-08-02 22:32:31 +0300

    Fix3

--- src/main.js
+++ src/main.js
@@ -111,7 +111,7 @@ scrollGalerry(cardHeight);
 
 function checkIsThereMoreToLoad(){
   const currentlyLoaded =  current_page * 15;
-  if(currentlyLoaded >= totalHits && totalHits != 0) {
+  if(currentlyLoaded >= totalHits || totalHits == 0) {
     
     iziToast.show({
       message: "We're sorry, but you've reached the end of search results.",
